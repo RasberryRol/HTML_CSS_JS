8d909047e0722041680ed9b40fa0e7c29b3c0670
Dief Gerard <dief9erard@gmail.com>
2025-01-09 10:14:29 -0600

    toDoList and weatherApp modifications

--- to-do-list-app/script.js
+++ to-do-list-app/script.js
@@ -1,10 +1,10 @@
-          
+         
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask(){
     if(inputBox.value === ''){
-        alert("You must write something!")
+        alert("You must write something!");
     }
     else{
         let li = document.createElement("li");
@@ -12,12 +12,10 @@ function addTask(){
         listContainer.appendChild(li);
 
         let span = document.createElement("span");
-        // add code formula for the cross icon
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
     inputBox.value = "";
-    // save data whenever we add a task
     saveData();
 }
 
@@ -30,14 +28,14 @@ listContainer.addEventListener("click", function(e){
         e.target.parentElement.remove();
         saveData();
     }
-}, false);
+}, false)
 
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-// display data when returning to browser after closure
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
+
 showTask();
